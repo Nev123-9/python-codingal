a11cb81d54d264eda970a090fc9fd379bfedcc9f
Nev123-9 <nadankatiah@gmail.com>
2026-04-01 20:20:13 -0400

    L4

---
+++ lesson 4/avrage.py
@@ -0,0 +1,11 @@
+tree1 = 98
+tree2 = 92
+tree3 =  90
+tree4 =   93
+tree5 =   94
+ 
+sum = tree1 + tree2 + tree3 + tree4 +  tree5
+print("the sum of all trees are :",sum)
+
+average = sum/ 5
+print("the average of all trees are:",average)
---
+++ lesson 4/percentage calculation.py
@@ -0,0 +1,11 @@
+print("enter marks of 3 subjects")
+
+maths = int(input("maths : "))
+science = int(input("science : "))
+computers = int(input("computers : "))
+
+sum = maths + science + computers
+perC = (sum/300) * 100
+print("percentage :",perC)
+
+
